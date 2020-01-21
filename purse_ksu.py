d6e7934c78191158dc02d0a60f9b0e2df4ac9b0e
Internet <netever@gmail.com>
2020-01-21 15:16:07 +0300

    edit purse_ksu module

--- purse_ksu.py
+++ purse_ksu.py
@@ -1,3 +1,4 @@
+import web
 from bs4 import BeautifulSoup
 
 def get_link(html):
@@ -5,9 +6,19 @@ def get_link(html):
     source = soup.find('div', id='k2Container').find_all(class_="catItemTitle")
     alldata = list()
     for i in source:
-        link = 'https://ksu.edu.ru/travel' + i.contents[1].get('href')
+        link = 'https://ksu.edu.ru' + i.contents[1].get('href')
         head = i.text
+        text = get_text(web.get(link))
         data = {'head': head,
-                 'link': link}
+                'link': link,
+                'text': text}
         alldata.append(data)
     return alldata
+
+def get_text(html):
+    soup = BeautifulSoup(html, 'lxml')
+    source = soup.find('div', id='k2Container').find_all('p')
+    text = ""
+    for i in source:
+        text = text + i.text
+    return text
